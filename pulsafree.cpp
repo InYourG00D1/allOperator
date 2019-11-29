476d6caf52199706ab6e12074f7dfb2354852f08
Ragil}{P <44290386+InYourG00D1@users.noreply.github.com>
2019-11-30 01:06:20 +0700

    Add files via upload

---
+++ pulsafree.cpp
@@ -0,0 +1,98 @@
+//Mau Yang Work? BIKIN AE SENDIRI TOOLNYA
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+using namespace std;
+int main(){
+int x;
+string p,s;
+	
+cout <<"       .---.          .-----------" <<endl;
+cout <<"      /     T  ___   /    ------" <<endl;
+cout <<"     / /     |(0 0) /    -----" <<endl;
+cout <<"    //////   *  Y  *   ---" <<endl;
+cout <<"   //// / // :     : ---" <<endl;
+cout <<"  // /   /  /*     *--" <<endl;
+cout <<" //          TT...TT" <<endl;
+cout <<" +==========UU====UU=====================Ragil+" <<endl;
+cout <<" +           *//||\|*                          +" <<endl;
+cout <<" +            ****                            +" <<endl;
+cout <<" + Coded by : InYourG00D                      +" <<endl;
+cout <<" + Team : LockNullXploit | Soul Team Cyber    +" <<endl;
+cout <<" + Github : https://github.com/InYourG00D1    +" <<endl;
+cout <<" +============================================+" <<endl;
+cout <<"           ^...^             ^...^" <<endl;
+cout <<"          <_* *_> HEK PULSA <_* *_>" <<endl;
+cout <<"            *Y*               *Y*\n" <<endl;
+cout <<"Pilih Operator"<<endl;
+cout << "1.Indosat     4.Axis" <<endl;
+cout << "2.Tree        5.XL"<<endl;
+cout << "3.Telkomsel   6.Smartfren" <<endl; 
+cout << "0.Out"
+cout << ": ";
+cin >> x;
+ switch (x){
+case 1:
+int a;
+cout << "Masukkan Nomor: \n" <<endl;
+cin >> p;
+cout << "Nominal Pulsa: " <<endl;
+cin >> s;
+while(a<2){
+cout << "Pulsa: " << s << " Telah Terkirim Ke Nomer: " << p <<endl;
+}case 2:
+int b;
+cout << "Masukkan Nomor: \n" <<endl;
+cin >> p;
+cout << "Nominal Pulsa: " <<endl;
+cin >> s;
+while(b<2){
+cout << "Pulsa: " << s << " Telah Terkirim Ke Nomer: " << p <<endl;
+}	 break;
+case 3: 
+int c;
+cout << "Masukkan Nomor: \n" <<endl;
+cin >> p;
+cout << "Nominal Pulsa: " <<endl;
+cin >> s;
+while(c<2){
+cout << "Pulsa: " << s << " Telah Terkirim Ke Nomer: " << p <<endl;
+} 	break;
+case 4:
+int d;
+cout << "Masukkan Nomor: \n" <<endl;
+cin >> p;
+cout << "Nominal Pulsa: " <<endl;
+cin >> s;
+while(d<2){
+cout << "Pulsa: " << s << " Telah Terkirim Ke Nomer: " << p <<endl;
+} 	break;
+case 5:
+int e;
+cout << "Masukkan Nomor: \n" <<endl;
+cin >> p;
+cout << "Nominal Pulsa: " <<endl;
+cin >> s;
+while(e<2){
+cout << "Pulsa: " << s << " Telah Terkirim Ke Nomer: " << p <<endl;
+} 	break;
+case 6:
+int f;
+cout << "Masukkan Nomor: \n" <<endl;
+cin >> p;
+cout << "Nominal Pulsa: " <<endl;
+cin >> s;
+while(f<2){
+cout << "Pulsa: " << s << " Telah Terkirim Ke Nomer: " << p <<endl;
+} 	break;
+case 0:
+cout << "Terimakasih Sudah Install Tool Ini" <<endl;
+cout << "Jangan Lupa Dishare :)" <<endl;
+cout << "Bye" <<endl;
+ 	default:
+        cout << "Maaf Tidak Terdaftar" <<endl;
+ }
+	return 0;
+}
